a60885d8a817424905a4eac481c5c26be72e2c18
Chris Kjellqvist <chriskjellqvist@gmail.com>
2025-12-09 19:41:24 -0500

    update to working

--- src/main/scala/systolic/verilog/SystolicArrayVerilogConfig_SOLUTION.scala
+++ src/main/scala/systolic/verilog/SystolicArrayVerilogConfig_SOLUTION.scala
@@ -21,12 +21,7 @@ class SystolicArrayConfig_SOLUTION(nCores: Int)
               EmptyAccelResponse()
             )
           ),
-          // This is where Beethoven will generate the AcceleratorCore
-          // definition if something is not already there with the correct
-          // name (it will be <SystemName>.v)
           sourcePath = os.pwd / "src" / "main" / "verilog" / "systolic",
-          // if the AcceleratorCore has verilog dependencies, they must all
-          // be elaborated here. If you're fancy, os.walk can scrape directories
           externalDependencies = {
             val src_dir = os.pwd / "src" / "main" / "verilog" / "systolic"
             Some(
@@ -37,7 +32,6 @@ class SystolicArrayConfig_SOLUTION(nCores: Int)
               )
             )
           },
-          // We can pass parameters to the Verilog module elaboration like this
           verilogMacroParams = Map(
             "SYSTOLIC_ARRAY_DIM" -> systolic_array_dim,
             "DATA_WIDTH_BITS" -> data_width_bits,
@@ -62,15 +56,9 @@ class SystolicArrayConfig_SOLUTION(nCores: Int)
       )
     )
 
-// The main method that will drive the elaboration of our accelerator
 object SystolicArrayConfig_SOLUTION
     extends BeethovenBuild(
       {
-        // usually you would just provide the config here, but because we want to generate
-        // some C++ #define statements with our accelerator configuration details, we put
-        // these statements here. This statement can be executed at any time but here is a
-        // decent place to put it. Typically, I put these statements inside the Chisel Core
-        // definition which is not applicable for Verilog
         CppGeneration.addPreprocessorDefinition(
           Seq(
             ("DATA_WIDTH_BYTES", Constants.data_width_bytes),
